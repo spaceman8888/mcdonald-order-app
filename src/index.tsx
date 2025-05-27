e4c36771d5f214b9e9ea4e58b54ca8f589a162e5
Burger <bobo920524@gamil.com>
2025-05-27 21:00:42 +0900

    fix : 1

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mcdonald-order-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
